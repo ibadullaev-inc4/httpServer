0b16cc02d50ee8d0ff5e1dc7727af677c0261012
Nariman Ibadullaev <n.ibadullaev@inc4.net>
2024-03-06 23:49:05 +0400

    How to write one-to-many relationships using SQLAlchemy

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import secrets
+
 from flask import Flask
 from flask_smorest import Api
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
-
-
+from flask_jwt_extended import JWTManager
 
 app=Flask(__name__)
 
@@ -17,6 +18,9 @@ app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 api = Api(app)
+
+app.config["JWT_SECRET_KEY"] = "267147144780067525750554488564863946480"
+jwt = JWTManager(app)
 
 api.register_blueprint(ItemBlueprint)
 api.register_blueprint(StoreBlueprint)
